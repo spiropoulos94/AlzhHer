2a34912251bb73f58444d7a6b3279dc545f6c6c9
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-10-30 17:41:04 +0200

    mobile menu fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@
 
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('icon-menu');
           $('.mobile-nav-overly').fadeOut();
         }
         return false;
@@ -73,12 +73,12 @@
       class: 'mobile-nav d-lg-none'
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icon-menu"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
     $(document).on('click', '.mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      $('.mobile-nav-toggle i').toggleClass('icon-cross');
       $('.mobile-nav-overly').toggle();
     });
 
@@ -93,7 +93,7 @@
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('icon-menu ');
           $('.mobile-nav-overly').fadeOut();
         }
       }
